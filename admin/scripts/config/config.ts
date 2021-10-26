bc7ff7b3a4fcb4a1daaea22dbfeed19ea3e83019
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2021-10-26 14:27:19 -0700

    Simplified remote config publish scripts

--- admin/scripts/config/config.ts
+++ admin/scripts/config/config.ts
@@ -1,15 +1,17 @@
 import { ServiceAccount } from "firebase-admin";
 import { publishRemoteConfig } from "./publishRemoteConfig";
 
-const publish = async () => {
-  const credentialsPath = "../../prod.credentials.json";
+const config = async () => {
+  const alias = process.argv[2]
+  const credentialsPath = `../../${alias}.credentials.json`;
   const credentials = await import(credentialsPath);
   try {
     await publishRemoteConfig(credentials as ServiceAccount);
-    console.log("Deployed Remote Configs for impowergames: phrases, terms");
+    const project = alias === "prod" ? "impowergames" : `impowergames-${alias}`
+    console.log(`Deployed Remote Configs for project: ${project}`);
   } catch (error) {
     console.error(error);
   }
 };
 
-publish();
+config();
