cec054c5bc04724c5bac8b9b03d9a2f761c7c91c
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2022-03-23 14:09:33 -0700

    getSectionAt should search using "from" not "to"

--- client/modules/impower-script-parser/utils/getSectionAt.ts
+++ client/modules/impower-script-parser/utils/getSectionAt.ts
@@ -8,7 +8,7 @@ export const getSectionAt = (
   const sectionEntries = Object.entries(result?.sections || {});
   for (let i = sectionEntries.length - 1; i >= 0; i -= 1) {
     const [id, section] = sectionEntries[i];
-    if (pos >= section.to) {
+    if (pos >= section.from) {
       return [id, section];
     }
   }
