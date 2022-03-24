6df642bea23068492979a62e3f1a9c65e213b5f8
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2022-03-23 20:16:13 -0700

    Add databaseURL to admin app config

--- client/lib/initAdminApp.ts
+++ client/lib/initAdminApp.ts
@@ -27,6 +27,7 @@ export const initAdminApp = async () => {
     credential: admin.credential.cert(
       JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY)
     ),
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
   };
   return admin.initializeApp(adminCredentials);
 };
