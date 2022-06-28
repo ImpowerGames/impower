02ffb55d1c85061eb7e8f01c7670c6a1639f23db
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2022-06-27 19:18:11 -0700

    Execute onReady whenever parsed

--- client/modules/impower-script-editor/components/ScriptEditor.tsx
+++ client/modules/impower-script-editor/components/ScriptEditor.tsx
@@ -507,7 +507,7 @@ const ScriptEditor = React.memo((props: ScriptEditorProps): JSX.Element => {
         EditorView.lineWrapping,
         EditorView.updateListener.of((u) => {
           const parsed = syntaxTreeAvailable(u.state);
-          if (parsed && !readyRef.current) {
+          if (parsed) {
             readyRef.current = true;
             onReadyRef.current?.(u);
             setReady(readyRef.current);
