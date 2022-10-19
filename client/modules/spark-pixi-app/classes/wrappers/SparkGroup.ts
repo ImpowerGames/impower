f1d1d5a5e94051c229ff06d39055ad8cc03324d3
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2022-10-19 16:49:21 -0700

    Remove useless constructors

--- client/modules/spark-pixi-app/classes/wrappers/SparkGroup.ts
+++ client/modules/spark-pixi-app/classes/wrappers/SparkGroup.ts
@@ -1,11 +1,3 @@
 import { Group } from "@pixi/layers";
-import { DisplayObject } from "pixi.js";
 
-export class SparkGroup extends Group {
-  constructor(
-    zIndex?: number,
-    sorting?: boolean | ((displayObject: DisplayObject) => void)
-  ) {
-    super(zIndex, sorting);
-  }
-}
+export class SparkGroup extends Group {}
--- client/modules/spark-pixi-app/classes/wrappers/SparkSprite.ts
+++ client/modules/spark-pixi-app/classes/wrappers/SparkSprite.ts
@@ -1,8 +1,3 @@
 import { Sprite3d } from "pixi-projection";
-import { Resource, Texture } from "pixi.js";
 
-export class SparkSprite extends Sprite3d {
-  constructor(texture: Texture<Resource>) {
-    super(texture);
-  }
-}
+export class SparkSprite extends Sprite3d {}
