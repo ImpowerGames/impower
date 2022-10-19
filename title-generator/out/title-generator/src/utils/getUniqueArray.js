7610a25053e38b60833547c8d86c789a8faa0916
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2022-10-19 16:18:08 -0700

    Remove out folder

--- title-generator/out/title-generator/src/utils/getUniqueArray.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getUniqueArray = void 0;
-const getUniqueArray = (arr) => {
-    return Array.from(new Set(arr.map((x) => x.toLowerCase()))).sort();
-};
-exports.getUniqueArray = getUniqueArray;
-//# sourceMappingURL=getUniqueArray.js.map
