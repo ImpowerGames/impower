9dfbc34bd09cef592a5f9776726b2f68ce33d794
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2022-10-24 20:20:28 -0700

    Remove preact and next-pwa to avoid memory leak

--- client/next.config.js
+++ client/next.config.js
@@ -3,71 +3,65 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
-const withPreact = require("next-plugin-preact");
-const withPWA = require("next-pwa");
 const webpack = require("webpack");
 
-module.exports = withBundleAnalyzer(
-  withPreact(
-    withPWA({
-      experimental: {
-        externalDir: true,
+module.exports = withBundleAnalyzer({
+  experimental: {
+    externalDir: true,
+  },
+  compiler: { emotion: true },
+  pwa: {
+    disable: process.env.NODE_ENV === "development",
+    dest: "public",
+    register: false,
+    skipWaiting: false,
+  },
+  webpack: (config, { isServer }) => {
+    config.module.rules.push({
+      test: /\.(svg)$/,
+      use: ["@svgr/webpack"],
+    });
+    config.module.rules.push({
+      test: /\.md$/,
+      loader: "raw-loader",
+    });
+    if (
+      !isServer &&
+      config.optimization.splitChunks.cacheGroups &&
+      config.optimization.splitChunks.cacheGroups.commons
+    ) {
+      config.optimization.splitChunks.cacheGroups.commons.minChunks = 60;
+    }
+    config.plugins = [
+      ...(config.plugins || []),
+      new webpack.ProvidePlugin({
+        PIXI: "pixi.js",
+      }),
+    ];
+    return config;
+  },
+  redirects: async () => {
+    return [
+      {
+        source: "/p",
+        destination: "/pitch",
+        permanent: true,
       },
-      compiler: { emotion: true },
-      pwa: {
-        disable: process.env.NODE_ENV === "development",
-        dest: "public",
-        register: false,
-        skipWaiting: false,
+      {
+        source: "/g",
+        destination: "/games",
+        permanent: true,
       },
-      webpack: (config, { isServer }) => {
-        config.module.rules.push({
-          test: /\.(svg)$/,
-          use: ["@svgr/webpack"],
-        });
-        config.module.rules.push({
-          test: /\.md$/,
-          loader: "raw-loader",
-        });
-        if (
-          !isServer &&
-          config.optimization.splitChunks.cacheGroups &&
-          config.optimization.splitChunks.cacheGroups.commons
-        ) {
-          config.optimization.splitChunks.cacheGroups.commons.minChunks = 60;
-        }
-        config.plugins = [
-          ...(config.plugins || []),
-          new webpack.ProvidePlugin({
-            PIXI: "pixi.js",
-          }),
-        ];
-        return config;
+      {
+        source: "/r",
+        destination: "/library",
+        permanent: true,
       },
-      redirects: async () => {
-        return [
-          {
-            source: "/p",
-            destination: "/pitch",
-            permanent: true,
-          },
-          {
-            source: "/g",
-            destination: "/games",
-            permanent: true,
-          },
-          {
-            source: "/r",
-            destination: "/library",
-            permanent: true,
-          },
-          {
-            source: "/e",
-            destination: "/engine",
-            permanent: true,
-          },
-        ];
+      {
+        source: "/e",
+        destination: "/engine",
+        permanent: true,
       },
-    })
-  )
-);
+    ];
+  },
+});
