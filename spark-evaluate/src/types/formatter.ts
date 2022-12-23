6be26c0553110759b8cba804531d769016063857
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2022-12-22 21:08:09 -0800

    Remove Formatter

--- spark-evaluate/src/types/formatter.ts
+++
@@ -1,7 +0,0 @@
-import { CompilerDiagnostic } from "./CompilerDiagnostic";
-import { CompilerReference } from "./CompilerReference";
-
-export type Formatter = (
-  str: string,
-  context?: Record<string, unknown>
-) => [string, CompilerDiagnostic[], CompilerReference[]];
