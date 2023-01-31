ec8b3a8ad4cfa2b9e32d161a32e9db69f0071184
Lovelle Cardoso <lovelle.therese.cardoso@gmail.com>
2023-01-30 22:56:13 -0800

    Cleanup roundToRatioDecimal

--- spark-beatmap/src/utils/roundToRatioDecimal.ts
+++ spark-beatmap/src/utils/roundToRatioDecimal.ts
@@ -1,17 +1,17 @@
 import { RATIOS } from "../constants/RATIOS";
 
+const closest = (arr: number[], goal: number) =>
+  arr.reduce(function (prev, curr) {
+    return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
+  });
+
 export const roundToRatioDecimal = (num: number): number => {
   const integer = Math.floor(num);
   const remainder = num - integer;
-  const closest = Object.keys(RATIOS)
-    .map((k) => Number(k))
-    .reduce(function (prev, curr) {
-      return Math.abs(curr - remainder) < Math.abs(prev - remainder)
-        ? curr
-        : prev;
-    });
-  if (closest === 0 || closest === 1) {
+  const ratioDecimals = Object.keys(RATIOS).map((k) => Number(k));
+  const roundedRemainder = closest(ratioDecimals, remainder);
+  if (roundedRemainder === 0 || roundedRemainder === 1) {
     return Math.round(num);
   }
-  return integer + closest;
+  return integer + roundedRemainder;
 };
