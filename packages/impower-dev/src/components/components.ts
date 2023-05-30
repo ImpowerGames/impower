859b65c0f391cf006f70f541c1553def14b7b81d
lovelle-cardoso <lovelle.cardoso@impower.games>
2023-05-30 15:26:22 -0700

    Add js file extensions

--- packages/impower-dev/src/components/components.ts
+++ packages/impower-dev/src/components/components.ts
@@ -1,5 +1,5 @@
-import sparkEditorComponents from "@impower/spark-editor/src/components";
-import sparkleComponents from "@impower/sparkle/src/components";
+import sparkEditorComponents from "@impower/spark-editor/src/components.js";
+import sparkleComponents from "@impower/sparkle/src/components.js";
 
 export default {
   ...sparkleComponents,
--- packages/impower-dev/src/pages/index.ts
+++ packages/impower-dev/src/pages/index.ts
@@ -1,4 +1,4 @@
-import SparkEditor from "@impower/spark-editor/src/index";
-import Sparkle from "@impower/sparkle/src/index";
+import SparkEditor from "@impower/spark-editor/src/index.js";
+import Sparkle from "@impower/sparkle/src/index.js";
 Sparkle.init();
 SparkEditor.init();
