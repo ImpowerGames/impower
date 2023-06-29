b33da368afba70e3073700e1bbcb6791f4007cd8
lovelle-cardoso <lovelle.cardoso@impower.games>
2023-06-28 20:29:06 -0700

    Fix scene time autocompletion

--- packages/sparkdown-language-server/src/sparkdown-language-server.ts
+++ packages/sparkdown-language-server/src/sparkdown-language-server.ts
@@ -9,180 +9,90 @@ import { TextDocument } from "vscode-languageserver-textdocument";
 import {
   BrowserMessageReader,
   BrowserMessageWriter,
-  CompletionItemKind,
-  InsertTextFormat,
-  InsertTextMode,
-  MarkupKind,
   createConnection,
 } from "vscode-languageserver/browser";
 
-import { SPARK_REGEX } from "../../sparkdown/src/constants/SPARK_REGEX";
 import {
   DidParseParams,
   DidParseTextDocument,
 } from "./classes/DidParseTextDocument";
 import SparkdownTextDocuments from "./classes/SparkdownTextDocuments";
 import getColorPresentations from "./utils/getColorPresentations";
+import getCompletions from "./utils/getCompletions";
 import getDocumentColors from "./utils/getDocumentColors";
 import getDocumentDiagnostics from "./utils/getDocumentDiagnostics";
-import getFencedCode from "./utils/getFencedCode";
 import getFoldingRanges from "./utils/getFoldingRanges";
-import getLineText from "./utils/getLineText";
-import getUniqueOptions from "./utils/getUniqueOptions";
 
 console.log("running sparkdown-language-server");
 
-const messageReader = new BrowserMessageReader(self);
-const messageWriter = new BrowserMessageWriter(self);
-const connection = createConnection(messageReader, messageWriter);
+try {
+  const messageReader = new BrowserMessageReader(self);
+  const messageWriter = new BrowserMessageWriter(self);
+  const connection = createConnection(messageReader, messageWriter);
 
-connection.onInitialize((_params: InitializeParams): InitializeResult => {
-  const capabilities: ServerCapabilities = {
-    foldingRangeProvider: true,
-    colorProvider: true,
-    completionProvider: {
-      resolveProvider: true,
-      triggerCharacters: [".", "\n", "\r", "-", " "],
-      completionItem: {
-        labelDetailsSupport: true,
+  connection.onInitialize((_params: InitializeParams): InitializeResult => {
+    const capabilities: ServerCapabilities = {
+      foldingRangeProvider: true,
+      colorProvider: true,
+      completionProvider: {
+        resolveProvider: true,
+        triggerCharacters: [".", "\n", "\r", "-", " "],
+        completionItem: {
+          labelDetailsSupport: true,
+        },
       },
-    },
-  };
-  return { capabilities };
-});
+    };
+    return { capabilities };
+  });
 
-const documents = new SparkdownTextDocuments(TextDocument);
+  const documents = new SparkdownTextDocuments(TextDocument);
 
-// parseProvider
-documents.onDidParse((change) => {
-  const params: DidParseParams = {
-    uri: change.document.uri,
-    version: change.document.version,
-    program: change.program,
-  };
-  connection.sendNotification(DidParseTextDocument.method, params);
-  connection.sendDiagnostics(
-    getDocumentDiagnostics(change.document, change.program)
-  );
-});
+  // parseProvider
+  documents.onDidParse((change) => {
+    const params: DidParseParams = {
+      uri: change.document.uri,
+      version: change.document.version,
+      program: change.program,
+    };
+    connection.sendNotification(DidParseTextDocument.method, params);
+    connection.sendDiagnostics(
+      getDocumentDiagnostics(change.document, change.program)
+    );
+  });
 
-// foldingRangeProvider
-connection.onFoldingRanges((params) => {
-  const uri = params.textDocument.uri;
-  const document = documents.get(uri);
-  const program = documents.program(uri);
-  return getFoldingRanges(document, program);
-});
-
-// colorProvider
-connection.onDocumentColor((params) => {
-  const uri = params.textDocument.uri;
-  const document = documents.get(uri);
-  const program = documents.program(uri);
-  return getDocumentColors(document, program);
-});
-connection.onColorPresentation((params) => {
-  return getColorPresentations(params.color);
-});
-
-// completionProvider
-connection.onCompletion((params: CompletionParams): CompletionItem[] => {
-  try {
+  // foldingRangeProvider
+  connection.onFoldingRanges((params) => {
     const uri = params.textDocument.uri;
     const document = documents.get(uri);
     const program = documents.program(uri);
-    if (!document) {
-      return [];
-    }
-    const lineText = getLineText(document, params.position);
-    const triggerCharacter = params.context?.triggerCharacter;
-    if (triggerCharacter === "\n" || triggerCharacter === "\r") {
-      return [];
-    }
-    const sceneMatch = lineText.match(SPARK_REGEX.scene);
-    if (triggerCharacter === " " && sceneMatch) {
-      const location = sceneMatch[3];
-      const dash = sceneMatch[5];
-      const time = sceneMatch[7];
-      if (!location) {
-        const locations = getUniqueOptions(
-          program?.metadata.scenes?.map((s) => s.location)
-        );
-        return locations.map((location) => ({
-          label: location,
-          kind: CompletionItemKind.Module,
-        }));
-      }
-      if (dash && !time) {
-        const times = getUniqueOptions([
-          ...(program?.metadata.scenes?.map((s) => s.time) || []),
-          "DAY",
-          "NIGHT",
-        ]);
-        return times.map((time) => ({
-          label: time,
-          kind: CompletionItemKind.Module,
-        }));
-      }
-    }
-    if (!triggerCharacter && !sceneMatch) {
-      return [
-        {
-          label: "INT.",
-          labelDetails: {
-            description: "Interior Scene",
-          },
-          detail: "An indoor scene",
-          documentation: {
-            kind: MarkupKind.Markdown,
-            value: getFencedCode(`INT. BEDROOM - NIGHT`),
-          },
-          insertText: "INT. ${1:LOCATION} - ${2:TIME}",
-          insertTextFormat: InsertTextFormat.Snippet,
-          insertTextMode: InsertTextMode.adjustIndentation,
-          kind: CompletionItemKind.Interface,
-        },
-        {
-          label: "EXT.",
-          labelDetails: {
-            description: "Exterior Scene",
-          },
-          detail: "An outdoor scene",
-          documentation: {
-            kind: MarkupKind.Markdown,
-            value: getFencedCode(`EXT. BEACH - DAY`),
-          },
-          insertText: "EXT. ${1:LOCATION} - ${2:TIME}",
-          insertTextFormat: InsertTextFormat.Snippet,
-          insertTextMode: InsertTextMode.adjustIndentation,
-          kind: CompletionItemKind.Interface,
-        },
-        {
-          label: "INT./EXT.",
-          labelDetails: {
-            description: "Intercut Scene",
-          },
-          detail: "A scene that is intercut between indoors and outdoors",
-          documentation: {
-            kind: MarkupKind.Markdown,
-            value: getFencedCode(`INT./EXT. PHONE BOOTH`),
-          },
-          insertText: "INT./EXT. ${1:LOCATION} - ${2:TIME}",
-          insertTextFormat: InsertTextFormat.Snippet,
-          insertTextMode: InsertTextMode.adjustIndentation,
-          kind: CompletionItemKind.Interface,
-        },
-      ];
-    }
-  } catch (e) {
-    console.error(e);
-  }
-  return [];
-});
-connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
-  return item;
-});
+    return getFoldingRanges(document, program);
+  });
 
-documents.listen(connection);
+  // colorProvider
+  connection.onDocumentColor((params) => {
+    const uri = params.textDocument.uri;
+    const document = documents.get(uri);
+    const program = documents.program(uri);
+    return getDocumentColors(document, program);
+  });
+  connection.onColorPresentation((params) => {
+    return getColorPresentations(params.color);
+  });
 
-connection.listen();
+  // completionProvider
+  connection.onCompletion((params: CompletionParams): CompletionItem[] => {
+    const uri = params.textDocument.uri;
+    const document = documents.get(uri);
+    const program = documents.program(uri);
+    return getCompletions(document, program, params.position, params.context);
+  });
+  connection.onCompletionResolve((item: CompletionItem): CompletionItem => {
+    return item;
+  });
+
+  documents.listen(connection);
+
+  connection.listen();
+} catch (e) {
+  console.error(e);
+}
---
+++ packages/sparkdown-language-server/src/utils/getCompletions.ts
@@ -0,0 +1,125 @@
+import {
+  CompletionContext,
+  CompletionItem,
+  CompletionItemKind,
+  InsertTextFormat,
+  InsertTextMode,
+  MarkupKind,
+  Position,
+} from "vscode-languageserver";
+import type { TextDocument } from "vscode-languageserver-textdocument";
+
+import { SPARK_REGEX } from "../../../sparkdown/src/constants/SPARK_REGEX";
+import type { SparkProgram } from "../../../sparkdown/src/types/SparkProgram";
+import getFencedCode from "./getFencedCode";
+import getLineText from "./getLineText";
+import getUniqueOptions from "./getUniqueOptions";
+
+const getSceneCompletions = () => {
+  return [
+    {
+      label: "INT.",
+      labelDetails: {
+        description: "Interior Scene",
+      },
+      detail: "An indoor scene",
+      documentation: {
+        kind: MarkupKind.Markdown,
+        value: getFencedCode(`INT. BEDROOM - NIGHT`),
+      },
+      insertText: "INT. ${1:LOCATION} - ${2:TIME}",
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertTextMode: InsertTextMode.adjustIndentation,
+      kind: CompletionItemKind.Interface,
+    },
+    {
+      label: "EXT.",
+      labelDetails: {
+        description: "Exterior Scene",
+      },
+      detail: "An outdoor scene",
+      documentation: {
+        kind: MarkupKind.Markdown,
+        value: getFencedCode(`EXT. BEACH - DAY`),
+      },
+      insertText: "EXT. ${1:LOCATION} - ${2:TIME}",
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertTextMode: InsertTextMode.adjustIndentation,
+      kind: CompletionItemKind.Interface,
+    },
+    {
+      label: "INT./EXT.",
+      labelDetails: {
+        description: "Intercut Scene",
+      },
+      detail: "A scene that is intercut between indoors and outdoors",
+      documentation: {
+        kind: MarkupKind.Markdown,
+        value: getFencedCode(`INT./EXT. PHONE BOOTH`),
+      },
+      insertText: "INT./EXT. ${1:LOCATION} - ${2:TIME}",
+      insertTextFormat: InsertTextFormat.Snippet,
+      insertTextMode: InsertTextMode.adjustIndentation,
+      kind: CompletionItemKind.Interface,
+    },
+  ];
+};
+const getSceneCaptureCompletions = (
+  match: string[],
+  program: SparkProgram | undefined
+) => {
+  const location = match[3];
+  const dash = match[5];
+  const time = match[7];
+  if (!location) {
+    const locations = getUniqueOptions(
+      program?.metadata.scenes?.map((s) => s.location)
+    );
+    return locations.map((location) => ({
+      label: location,
+      kind: CompletionItemKind.Module,
+    }));
+  }
+  if (dash && !time) {
+    const times = getUniqueOptions([
+      ...(program?.metadata.scenes?.map((s) => s.time) || []),
+      "DAY",
+      "NIGHT",
+    ]);
+    return times.map((time) => ({
+      label: time,
+      kind: CompletionItemKind.Module,
+    }));
+  }
+  return [];
+};
+
+const getCompletions = (
+  document: TextDocument | undefined,
+  program: SparkProgram | undefined,
+  position: Position,
+  context: CompletionContext | undefined
+): CompletionItem[] => {
+  if (!document) {
+    return [];
+  }
+  const lineText = getLineText(document, position);
+  const triggerCharacter = context?.triggerCharacter;
+  if (triggerCharacter === "\n" || triggerCharacter === "\r") {
+    return [];
+  }
+  const lineMetadata = program?.metadata?.lines?.[position?.line];
+  const scopeName = program?.scopes?.[lineMetadata?.scope ?? -1];
+  if (!scopeName) {
+    const sceneMatch = lineText.match(SPARK_REGEX.scene);
+    if (triggerCharacter === " " && sceneMatch) {
+      return getSceneCaptureCompletions(sceneMatch, program);
+    }
+    if (!triggerCharacter && !sceneMatch) {
+      return getSceneCompletions();
+    }
+  }
+  return [];
+};
+
+export default getCompletions;
