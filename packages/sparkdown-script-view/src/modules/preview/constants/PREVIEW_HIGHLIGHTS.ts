a27661dcdfda18a705c6b6c66f2a05eb553110b2
lovelle-cardoso <lovelle.cardoso@impower.games>
2023-07-03 16:06:21 -0700

    Fix invalid capture length

--- packages/sparkdown-script-view/src/modules/preview/constants/PREVIEW_HIGHLIGHTS.ts
+++ packages/sparkdown-script-view/src/modules/preview/constants/PREVIEW_HIGHLIGHTS.ts
@@ -3,9 +3,32 @@ import { tags } from "@lezer/highlight";
 
 const PREVIEW_HIGHLIGHTS = HighlightStyle.define([
   {
-    tag: tags.comment,
-    display: "none",
+    tag: tags.contentSeparator,
+    "&:after": {
+      content: "",
+      textDecoration: "line-through",
+      color: "#333",
+    },
   },
+
+  { tag: tags.quote, fontStyle: "italic" },
+  { tag: tags.emphasis, fontStyle: "italic" },
+  { tag: tags.strong, fontWeight: "bold" },
+  {
+    tag: tags.link,
+    textDecoration: "underline",
+    textUnderlineOffset: "5px",
+  },
+  {
+    tag: tags.strikethrough,
+    textDecoration: "line-through",
+  },
+
+  { tag: tags.definition(tags.heading), opacity: 0.5 },
+
+  { tag: tags.docComment, opacity: 0.5 },
+  { tag: tags.lineComment, display: "none" },
+  { tag: tags.blockComment, display: "none" },
 ]);
 
 export default PREVIEW_HIGHLIGHTS;
