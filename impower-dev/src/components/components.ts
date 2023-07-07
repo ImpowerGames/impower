34f7e337129c550774df37ecaa3c5f224d0372b6
lovelle-cardoso <lovelle.cardoso@impower.games>
2023-07-07 15:10:02 -0700

    Fix package name

--- impower-dev/src/components/components.ts
+++ impower-dev/src/components/components.ts
@@ -1,7 +1,7 @@
 import sparkEditorComponents from "@impower/spark-editor/src/components.js";
 import sparkElementComponents from "@impower/spark-element/src/components.js";
-import sparkdownScriptEditorComponents from "@impower/sparkdown-script-view/src/modules/editor/components.js";
-import sparkdownScriptPreviewComponents from "@impower/sparkdown-script-view/src/modules/preview/components.js";
+import sparkdownScriptEditorComponents from "@impower/sparkdown-script-views/src/modules/editor/components.js";
+import sparkdownScriptPreviewComponents from "@impower/sparkdown-script-views/src/modules/preview/components.js";
 import sparkleComponents from "@impower/sparkle/src/components.js";
 
 export default {
