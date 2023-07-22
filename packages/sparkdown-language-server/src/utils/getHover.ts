c7f3f548deef0174ed92a68f12061a4fc98881b4
lovelle-cardoso <lovelle.cardoso@impower.games>
2023-07-21 22:01:29 -0700

    Remove unused function

--- packages/sparkdown-language-server/src/utils/getHover.ts
+++ packages/sparkdown-language-server/src/utils/getHover.ts
@@ -1,29 +1,7 @@
-import {
-  CompletionItemKind,
-  Hover,
-  MarkupKind,
-  Position,
-} from "vscode-languageserver";
+import { Hover, MarkupKind, Position } from "vscode-languageserver";
 import type { TextDocument } from "vscode-languageserver-textdocument";
 
 import type { SparkProgram } from "@impower/sparkdown/src/types/SparkProgram";
-
-const getImageCompletions = (program: SparkProgram | undefined) => {
-  if (!program) {
-    return [];
-  }
-  return Object.entries(program?.objectMap?.["image"] || {}).map(
-    ([name, { src, type }]) => ({
-      label: name,
-      labelDetails: { description: type },
-      kind: CompletionItemKind.Constructor,
-      documentation: {
-        kind: MarkupKind.Markdown,
-        value: `![${name}](${src})`,
-      },
-    })
-  );
-};
 
 const getHover = (
   document: TextDocument | undefined,
