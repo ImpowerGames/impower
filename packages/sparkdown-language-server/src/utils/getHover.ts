e8a0c46759fd4502972dae240f8185e5dc25ff74
lovelle-cardoso <lovelle.cardoso@impower.games>
2023-11-28 18:45:08 -0800

    Remove console.log

--- packages/sparkdown-language-server/src/utils/getHover.ts
+++ packages/sparkdown-language-server/src/utils/getHover.ts
@@ -2,6 +2,7 @@ import { Hover, MarkupKind, Position } from "vscode-languageserver";
 import type { TextDocument } from "vscode-languageserver-textdocument";
 
 import type { SparkProgram } from "@impower/sparkdown/src/types/SparkProgram";
+import type { SparkReference } from "@impower/sparkdown/src/types/SparkReference";
 import getFencedCode from "./getFencedCode";
 import { isAssetOfType } from "./isAsset";
 
@@ -15,7 +16,7 @@ const getHover = (
     return null;
   }
   for (let i = 0; i < references.length; i += 1) {
-    const reference = references[i]!;
+    const reference = references[i]! as SparkReference;
     const range = {
       start: document.positionAt(reference.from),
       end: document.positionAt(reference.to),
@@ -40,6 +41,19 @@ const getHover = (
         if (section) {
           const fencedCode = getFencedCode(
             `${"".padStart(section.level, "#")} ${section.name}`
+          );
+          return {
+            contents: {
+              kind: MarkupKind.Markdown,
+              value: fencedCode,
+            },
+            range,
+          };
+        }
+        const variable = program.variables?.[id];
+        if (variable) {
+          const fencedCode = getFencedCode(
+            `: ${JSON.stringify(variable?.compiled)}`
           );
           return {
             contents: {
