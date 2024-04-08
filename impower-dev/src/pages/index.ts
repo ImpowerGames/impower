088702b7699df61919a973dc9a74899d74735d62
lovelle-cardoso <lovelle.cardoso@impower.games>
2024-04-08 14:54:20 -0700

    Remove console warning

--- impower-dev/src/pages/index.ts
+++ impower-dev/src/pages/index.ts
@@ -14,7 +14,6 @@ const load = async () => {
   Object.entries(svgs).forEach(([name, svg]) => {
     graphics[name] = svg;
   });
-  console.warn("svgs", svgs);
   await Promise.allSettled([
     Sparkle.init({ graphics }),
     SparkWebPlayer.init({ graphics }),
