a8ac9ecfabb76f78754e8b24ffeb4fc310894932
lovelle-cardoso <lovelle.cardoso@impower.games>
2024-08-18 12:53:57 -0700

    Start at base pitch level

--- packages/spark-engine/src/game/modules/interpreter/utils/stressPhrases.ts
+++ packages/spark-engine/src/game/modules/interpreter/utils/stressPhrases.ts
@@ -34,8 +34,7 @@ export const stressPhrases = (
 ): void => {
   const stressLevelMultiplier = 0.5;
   const lineIncrement = 1;
-  // Speaker starts at max pitch and ends at their natural speaking pitch once arriving at the point of their speech.
-  let lineLevel = (phrases.length - 1) * lineIncrement;
+  let lineLevel = 0;
   phrases.forEach((phrase) => {
     if (phrase.text?.trim()) {
       const chunks = phrase.chunks;
