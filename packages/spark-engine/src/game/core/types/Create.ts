f7c8db9ebfc6c57fc7fa0f439d3fe27cb35e04b2
lovelle-cardoso <lovelle.cardoso@impower.games>
2024-10-16 14:11:24 -0700

    Fix Create type

--- packages/spark-engine/src/game/core/types/Create.ts
+++ packages/spark-engine/src/game/core/types/Create.ts
@@ -1,4 +1,12 @@
-import { RecursivePartial } from "./RecursivePartial";
+export type RecursivePartial<T> = {
+  [P in keyof T]?: T[P] extends (infer U)[]
+    ? U[]
+    : T[P] extends Function
+    ? T[P]
+    : T[P] extends object
+    ? RecursivePartial<T[P]>
+    : T[P];
+};
 
 export type Create<T = any> = (
   obj: RecursivePartial<T> & { $type?: string; $name: string }
