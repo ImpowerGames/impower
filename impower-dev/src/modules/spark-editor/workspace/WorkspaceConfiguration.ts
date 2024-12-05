ed918513e5c17c4648c7fcc8327245c819ab6371
lovelle-cardoso <lovelle.cardoso@impower.games>
2024-12-04 20:14:47 -0800

    Support more asset file extensions

--- impower-dev/src/modules/spark-editor/workspace/WorkspaceConfiguration.ts
+++ impower-dev/src/modules/spark-editor/workspace/WorkspaceConfiguration.ts
@@ -1,8 +1,8 @@
 export default class WorkspaceConfiguration {
   protected _settings = {
     scriptFiles: "*.{sparkdown,sd,script,project}",
-    imageFiles: "*.{png,apng,jpeg,jpg,gif,bmp,svg}",
-    audioFiles: "*.{mid,wav,mp3,ogg}",
+    imageFiles: "*.{png,apng,jpeg,jpg,gif,bmp,svg,webp}",
+    audioFiles: "*.{mid,wav,mp3,mp2,ogg,aac,opus,flac}",
     fontFiles: "*.{ttf,woff,woff2,otf}",
   };
   get settings() {
