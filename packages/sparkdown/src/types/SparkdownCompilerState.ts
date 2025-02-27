f5761db8e0c734b9006cfc9d2c6d2a82a3d1a555
lovelle-cardoso <lovelle.cardoso@impower.games>
2025-02-26 20:49:56 -0600

    Reorder state properties

--- packages/sparkdown/src/types/SparkdownCompilerState.ts
+++ packages/sparkdown/src/types/SparkdownCompilerState.ts
@@ -2,14 +2,14 @@ import { SparkLocation } from "./SparkLocation";
 import { SparkTranspilationOffset } from "./SparkTranspilationOffset";
 
 export interface SparkdownCompilerState {
-  implicitDefs?: {
-    [type: string]: { [name: string]: any };
-  };
-  properties?: Record<string, SparkLocation[]>;
   transpiledScripts?: {
     [uri: string]: string;
   };
   sourceMap?: {
     [uri: string]: { [line: number]: SparkTranspilationOffset };
+  };
+  properties?: Record<string, SparkLocation[]>;
+  implicitDefs?: {
+    [type: string]: { [name: string]: any };
   };
 }
