35d8719a24545be68ff41695943d0122479e89da
lovelle-cardoso <lovelle.cardoso@impower.games>
2025-05-07 15:00:27 -0500

    Shorthands are expanded when converting to css variable, so no need to get shorthand variable

--- packages/sparkle-style-transformer/src/utils/transformers.ts
+++ packages/sparkle-style-transformer/src/utils/transformers.ts
@@ -533,17 +533,11 @@ export const getCssPosition = (value: string): string => {
   if (value === "default") {
     return "static";
   }
-  if (value.startsWith("sticky")) {
+  if (value.startsWith("sticky-")) {
     return "sticky";
   }
-  if (value.startsWith("absolute")) {
-    return "absolute";
-  }
-  if (value.startsWith("fixed")) {
+  if (value.startsWith("fixed-")) {
     return "fixed";
-  }
-  if (value.startsWith("relative")) {
-    return "relative";
   }
   return value;
 };
