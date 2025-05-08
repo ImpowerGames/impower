cff58275446a180bfb6944e548a80ba011bb66e7
lovelle-cardoso <lovelle.cardoso@impower.games>
2025-05-08 14:37:48 -0500

    Move sparkle-element focus styles to core.css

--- packages/sparkle/src/styles/shared.ts
+++ packages/sparkle/src/styles/shared.ts
@@ -1,9 +1,11 @@
 import baseNormalize from "../../../spec-component/src/styles/normalize/normalize.css";
 import keyframes from "../styles/keyframes/keyframes.css";
 import normalize from "../styles/normalize/normalize.css";
+import core from "../styles/utility/core.css";
 import utility from "../styles/utility/utility.css";
 import scopeCssToHost from "../utils/scopeCssToHost";
 
 const scopedUtility = scopeCssToHost(utility);
+const scopedCore = scopeCssToHost(core);
 
-export default [baseNormalize, normalize, keyframes, scopedUtility];
+export default [baseNormalize, normalize, keyframes, scopedUtility, scopedCore];
