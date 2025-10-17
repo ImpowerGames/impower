d8982cb5d6824701ab3c55bcef5ca64d56dccd2e
lovelle-cardoso <lovelle.cardoso@impower.games>
2025-10-17 10:40:48 -0500

    Don't sort name if doesn't include filters

--- packages/sparkdown/src/utils/sortFilteredName.ts
+++ packages/sparkdown/src/utils/sortFilteredName.ts
@@ -1,5 +1,8 @@
-export const sortFilteredName = (nameWithFilters: string) => {
-  const parts = nameWithFilters.split("~");
+export const sortFilteredName = (name: string) => {
+  if (!name.includes("~")) {
+    return name;
+  }
+  const parts = name.split("~");
   const [fileName, ...filterNames] = parts;
   const sortedFilterNames = filterNames.sort();
   return [fileName, ...sortedFilterNames].join("~");
