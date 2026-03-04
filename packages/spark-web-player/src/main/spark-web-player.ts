436c68a41635211094f955ed77d7fe9bb01a0ecd
lovelle-cardoso <lovelle.cardoso@impower.games>
2026-03-04 16:55:23 -0500

    Don't fail simulation when rebuilding

--- packages/spark-web-player/src/main/spark-web-player.ts
+++ packages/spark-web-player/src/main/spark-web-player.ts
@@ -1505,7 +1505,9 @@ export default class SparkWebPlayer extends Component(spec) {
         const simulateFromPath = Game.getSimulateFromPath(validPreviewPath);
         this._game.simulatePath = simulateFromPath;
       }
-      this._game.simulation = "fail";
+      if (!shouldBuildNewGame) {
+        this._game.simulation = "fail";
+      }
     }
 
     if (shouldBuildNewGame) {
