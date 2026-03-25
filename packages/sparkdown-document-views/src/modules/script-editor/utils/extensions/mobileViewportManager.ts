aed09f72ab7a077f89db1f9e268fe3bb67916c23
lovelle-cardoso <lovelle.cardoso@impower.games>
2026-03-25 06:35:59 -0400

    Remove early out since it is causing problems on safari

--- packages/sparkdown-document-views/src/modules/script-editor/utils/extensions/mobileViewportManager.ts
+++ packages/sparkdown-document-views/src/modules/script-editor/utils/extensions/mobileViewportManager.ts
@@ -46,35 +46,6 @@ const viewportPlugin = ViewPlugin.fromClass(
       // when the input is focused.
       window.scrollTo(0, 0);
 
-      // Get the root (either document or the ShadowRoot)
-      const root = this.view.dom.getRootNode() as Document | ShadowRoot;
-
-      // Check if focus is in the main text area OR inside any editor panel/widget
-      // We check both the global activeElement and the ShadowRoot's activeElement
-      const activeElt = root.activeElement;
-      const isActiveEltEditable =
-        activeElt &&
-        (activeElt instanceof HTMLInputElement ||
-          activeElt instanceof HTMLTextAreaElement ||
-          (activeElt as HTMLElement).isContentEditable);
-      const isEditorInputFocused =
-        this.view.hasFocus ||
-        (activeElt && this.view.dom.contains(activeElt) && isActiveEltEditable);
-
-      const isFocusEvent = e?.type === "focusin" || e?.type === "focus";
-
-      const isBlurEvent = e?.type === "focusout" || e?.type === "blur";
-
-      // If the editor is losing focus or doesn't currently have focus,
-      // release the height constraint and ignore the trailing resize events
-      // from the keyboard animation.
-      if (!isFocusEvent && (isBlurEvent || !isEditorInputFocused)) {
-        document.body.style.height = "";
-        document.documentElement.classList.remove("keyboard-open");
-        this.lastKeyboardHeight = 0;
-        return;
-      }
-
       // We only reach here if the editor has focus, so we can safely
       // lock the body height to the visual viewport.
       document.body.style.height = `${vv.height}px`;
