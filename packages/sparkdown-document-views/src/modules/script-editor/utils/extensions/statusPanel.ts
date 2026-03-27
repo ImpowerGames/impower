94413cd5eb0c2248610f704bf8171911a7af5c49
lovelle-cardoso <lovelle.cardoso@impower.games>
2026-03-27 12:35:40 -0400

    Try scrolling to top after animation frame

--- packages/sparkdown-document-views/src/modules/script-editor/utils/extensions/statusPanel.ts
+++ packages/sparkdown-document-views/src/modules/script-editor/utils/extensions/statusPanel.ts
@@ -85,17 +85,14 @@ export class StatusPanel implements Panel {
     this.revealBottomPanelButton.onpointerdown = () => {
       this.previouslyFocused = view.hasFocus;
     };
-    this.revealBottomPanelButton.onfocus = () => {
-      if (this.previouslyFocused) {
-        view.focus();
-        window.scrollTo(0, 0);
-      }
-    };
     this.revealBottomPanelButton.onclick = () => {
       this.toggleBottomPanel(view.state);
       if (this.previouslyFocused) {
         view.focus();
         window.scrollTo(0, 0);
+        window.requestAnimationFrame(() => {
+          window.scrollTo(0, 0);
+        });
       }
     };
 
